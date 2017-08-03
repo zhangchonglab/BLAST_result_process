79c1f59bbb57423019257562e65a177e0e622a05
Chong Zhang <30624999+zhangchonglab@users.noreply.github.com>
2017-08-04 04:11:40 +0800

    Update MappingExtract.py

--- MappingExtract.py
+++ MappingExtract.py
@@ -1,6 +1,11 @@
-# this script utilized a blastn/p outfmt6 file and database file to extract the seq that 5' and 3' along the query
-# input1 blast outfmt6
-# databse fasta
+# this script utilized a blastn/p outfmt6 file and database file to extract the exactly mapped seq along the query according to the BLAST search result.
+# Several constant nucleotides (amino acids) can be also extracted, the number of which is specified by upstream_number and downstream_number
+# utility: python MappingExtract.py blast_result db upstream_number downstream_number prefix
+# blast_result output file of BLAST+ package (BLASTP, BLASTN, etc) in outfmt6 format
+# db: databse fasta file used in the abovementioned BLAST search
+# upstream_number: number of nucl or aa upstream of query, integer
+# downstream_number: number of nucl or aa downstream of query, integer
+# prefix: prefix to name the output files
 
 import os
 import sys
@@ -8,10 +13,7 @@ from Bio.Seq import Seq
 
 blastResult=sys.argv[1]
 fastaFile=sys.argv[2]
-
-# number of nucl or aa upstream of query
 Upstream=int(sys.argv[3])
-# number of nucl or aa downstream of query
 Downstream=int(sys.argv[4])
 prefix=sys.argv[5]
 
